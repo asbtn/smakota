bf198ff7f23b127845356772cd4d899bf652313f
asbtn <alisa.sbtn@proton.me>
2025-03-06 19:30:09 +0200

    fix: typo in landing_url

--- test/controllers/landings_controller_test.rb
+++ test/controllers/landings_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class LandingsControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get landings_show_url
+    get landing_url
 
     assert_response :success
   end
