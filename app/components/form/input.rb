d8d04a2af742cc75ab1814c16fff16abdab3dc33
asbtn <alisa.sbtn@proton.me>
2025-03-29 20:27:45 +0200

    refactor: refactor phlex components

--- app/components/form/input.rb
+++ app/components/form/input.rb
@@ -14,24 +14,12 @@ class Components::Form::Input < Components::Form::Base
       render Components::Form::Label.new(form: form, attribute: attribute)
 
       div class: "group w-full relative inline-block focus:ring-3 focus:outline-hidden" do
-        span class: "absolute inset-0 translate-x-1 translate-y-1 transition-transform group-hover:translate-x-0 group-hover:translate-y-0 #{ error ? 'bg-rose-100' : 'bg-mindaro-100' }"
-
-        raw safe(
-          form.public_send(:"#{field}_field",
-            attribute,
-            value: object&.public_send(attribute),
-            class: "w-full relative inline-block border-1 border-black p-4 pe-12 text-sm shadow-sm #{ error ? 'shadow-rose-300' : 'shadow-mindaro-300' }",
-            placeholder: t(".#{attribute}"),
-            required: required
-          )
-        )
-
-        span class: "absolute inset-y-0 end-0 grid place-content-center px-4" do
-          inline_svg_tag "icons/#{field}.svg", class: error ? "text-rose-700" : "text-mindaro-700" if icon
-        end
+        render_backdrop
+        render_field
+        render_icon if icon
       end
 
-      small(class: "text-rose-300 italic") { error.first } if error.present?
+      render_error if error.present?
     end
   end
 
@@ -48,5 +36,31 @@ class Components::Form::Input < Components::Form::Base
 
       errors[attribute].presence
     end
+  end
+
+  def render_backdrop
+    span class: "absolute inset-0 translate-x-1 translate-y-1 transition-transform group-hover:translate-x-0 group-hover:translate-y-0 #{ error ? 'bg-rose-100' : 'bg-mindaro-100' }"
+  end
+
+  def render_field
+    raw safe(
+          form.public_send(:"#{field}_field",
+            attribute,
+            value: object&.public_send(attribute),
+            class: "w-full relative inline-block border-1 border-black p-4 pe-12 text-sm shadow-sm #{ error ? 'shadow-rose-300' : 'shadow-mindaro-300' }",
+            placeholder: t(".#{attribute}"),
+            required: required
+          )
+        )
+  end
+
+  def render_icon
+    span class: "absolute inset-y-0 end-0 grid place-content-center px-4" do
+      inline_svg_tag "icons/#{field}.svg", class: error ? "text-rose-700" : "text-mindaro-700"
+    end
+  end
+
+  def render_error
+    small(class: "text-rose-300 italic") { error.first }
   end
 end
--- app/views/passwords/new.rb
+++ app/views/passwords/new.rb
@@ -5,28 +5,48 @@ class Views::Passwords::New < Views::Base
 
   def view_template
     section class: "relative flex flex-wrap lg:h-screen lg:items-center" do
-      div class: "relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2 items-end" do
-        image_tag "storyset/Forgot password-bro.svg", class: "absolute inset-0 h-full w-full object-cover"
-        render Components::StorysetAttribution.new
+      render_left_half
+      render_right_half
+    end
+  end
+
+  private
+
+  def render_left_half
+    div class: "relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2 items-end" do
+      image_tag "storyset/Forgot password-bro.svg", class: "absolute inset-0 h-full w-full object-cover"
+      render Components::StorysetAttribution.new
+    end
+  end
+
+  def render_right_half
+    div class: "w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24" do
+      div class: "mx-auto md:w-2/3 w-full" do
+        render Components::Typography::Title.new t(".title")
+
+        render_form
+      end
+    end
+  end
+
+  def render_form
+    form_with url: passwords_path, class: "mb-0 max-w-md space-y-4 justify-center" do |form|
+      render_form_inputs(form)
+      render_form_footer(form)
+    end
+  end
+
+  def render_form_inputs(form)
+    render Components::Form::Input.new(form:, attribute: :email_address, field: :email, icon: true, required: true)
+  end
+
+  def render_form_footer(form)
+    div class: "flex items-center justify-between" do
+      p class: "text-sm text-gray-500" do
+        a(href: new_session_path, class: "underline") { t ".login" }
       end
 
-      div class: "w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24" do
-        div class: "mx-auto md:w-2/3 w-full" do
-          render Components::Typography::Title.new t(".title")
-
-          form_with url: passwords_path, class: "mb-0 max-w-md space-y-4 justify-center" do |form|
-            render Components::Form::Input.new(form:, attribute: :email_address, field: :email, icon: true, required: true)
-
-            div class: "flex items-center justify-between" do
-              p class: "text-sm text-gray-500" do
-                a(href: new_session_path, class: "underline") { t ".login" }
-              end
-
-              render Components::Form::Submit.new(form:, action: :login)
-            end
-          end
-        end
-      end
+      render Components::Form::Submit.new(form:, action: :login)
     end
   end
 end
