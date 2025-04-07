16ad4acdec3d180ba6870bfc0467d4f110a6a84e
asbtn <alisa.sbtn@proton.me>
2025-04-07 17:06:01 +0300

    chore: add annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,44 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email_address   :string           not null
+#  name            :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email_address  (email_address) UNIQUE
+#
 class User < ApplicationRecord
+  # == Extensions ===========================================================
+  has_secure_password
+
+  # == Constants ============================================================
   PASSWORD_REGEX = /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}\z/
 
-  has_secure_password
+  # == Attributes ===========================================================
+
+  # == Relationships ========================================================
   has_many :sessions, dependent: :destroy
 
-  normalizes :email_address, with: ->(e) { e.strip.downcase }
-
-  validates :email_address, presence: true,
-                            format: { with: URI::MailTo::EMAIL_REGEXP },
-                            length: { minimum: 3 }
-
-  validates :name, presence: true,
-                   length: { minimum: 3, maximum: 256 }
-
+  # == Validations ==========================================================
+  validates :email_address, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { minimum: 3 },
+                            uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: { minimum: 3, maximum: 256 }
   validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX }
+
+  # == Scopes ===============================================================
+
+  # == Callbacks ============================================================
+
+  # == Class Methods ========================================================
+
+  # == Instance Methods =====================================================
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
